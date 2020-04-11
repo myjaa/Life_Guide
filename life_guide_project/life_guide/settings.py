c35d2e1c8355ba75694c4082d0d5d3f06d526053
Mohammad yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-04-11 19:10:39 +0530

    commit 3

--- life_guide_project/life_guide/settings.py
+++ life_guide_project/life_guide/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'life_guide.urls'
